a26354e5574bb4d72a57fd71c85f363f911636f1
Jack Coleman <45573555+Jack-coleman28@users.noreply.github.com>
2019-01-30 16:40:28 -0800

    Create tower.h

---
+++ tower.h
@@ -0,0 +1,13 @@
+#ifndef TOWER_H
+#define TOWER_H
+
+#include <stdio.h>
+#include <math.h>
+
+enum colors { BLACK, RED, GREEN, YELLOW, BLUE, PURPLE, CYAN, WHITE, GREY = 60 };
+
+void towerMoves(int nlocal, char L, char M, char R);
+void text_color(i);
+void back_color(i);
+
+#endif
